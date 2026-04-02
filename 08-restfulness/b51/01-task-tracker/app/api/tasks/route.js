4f7f8828dfe697f80052232e9b3cc2922e7ec4ff
Georges <georges.younes@gmail.com>
2026-04-02 22:49:09 +0300

    use localeCompare when sorting strings in 08-restfulness

--- 08-restfulness/b51/01-task-tracker/app/api/tasks/route.js
+++ 08-restfulness/b51/01-task-tracker/app/api/tasks/route.js
@@ -2,15 +2,20 @@ import * as repo from "@/repo/tasks";
 
 export async function GET(request) {
   const tasks = await repo.get();
-  const sorted = request.nextUrl.searchParams.get("sorted"); // query parameters
+  // const sorted = request.nextUrl.searchParams.get("sorted"); // query parameters
+  const sorted = new URL(request.url).searchParams.get("sorted"); // query parameters
 
   if (sorted === "asc") {
-    tasks.sort((a, b) => (a.id < b.id ? -1 : a.id === b.id ? 0 : +1));
+    // tasks.sort((a, b) => (a.id < b.id ? -1 : a.id === b.id ? 0 : +1));
+    tasks.sort((a, b) => a.title.localeCompare(b.title));
   } else if (sorted === "desc") {
-    tasks.sort((a, b) => (a.id > b.id ? -1 : a.id === b.id ? 0 : +1));
+    // tasks.sort((a, b) => (a.id > b.id ? -1 : a.id === b.id ? 0 : +1));
+    tasks.sort((a, b) => b.title.localeCompare(a.title));
   }
 
   return Response.json(tasks, { status: 200 });
+
+  // const response = await fetch();
 
   // const path = "repo/data/tasks.json";
   // return await Bun.file(path).json();
@@ -27,4 +32,5 @@ export async function POST(request) {
     return Response.json(task, { status: 201 });
   }
   return Response.json({ error: "Bad request." }, { status: 400 });
+  // return new Response("<html><body><p>Paragraph</p></body></html>")
 }
